e927f5c1171756091699e50234f86d311fc19824
Stanislav <77130336+standbit@users.noreply.github.com>
2022-03-13 11:07:12 +0300

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+from ast import excepthandler
+import pathlib
+
 import requests
 
 
-filename = "dvmn.svg"
-url = "https://dvmn.org/filer/canonical/1542890876/16/"
-response = requests.get(url)
-response.raise_for_status()
-with open(filename, "wb") as outfile:
-    outfile.write(response.content)
+def download_books(books_num):
+    books_folder = pathlib.Path("./books/")
+    books_folder.mkdir(parents=True, exist_ok=True)
+    for num in range(1, books_num + 1):
+        filename = f"{books_folder}//id{num}.txt"
+        url = f"http://tululu.org/txt.php?id={num}"
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+        with open(filename, "w") as outfile:
+            outfile.write(response.text)
+    
+
+def main():
+    try:
+        download_books(10)
+    except requests.exceptions.HTTPError as err:
+        print("General Error, incorrect link\n", str(err))
+    except requests.ConnectionError as err:
+        print("Connection Error. Check Internet connection.\n", str(err))
+
+
+if __name__=="__main__":
+    main()
