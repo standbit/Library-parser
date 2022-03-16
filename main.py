47c7952e90aec5f212182c86bd4555fda2f82c7b
Stanislav <77130336+standbit@users.noreply.github.com>
2022-03-17 00:01:24 +0300

    Download books with titles

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import urllib3
 
 import requests
 from pathvalidate import sanitize_filename
+from tululu import get_book_title
 
 
 def check_for_redirect(response):
@@ -13,53 +14,33 @@ def check_for_redirect(response):
     pass
 
 
-def download_books(books_num):
-    books_folder = pathlib.Path("./books/")
-    books_folder.mkdir(parents=True, exist_ok=True)
-    for num in range(1, books_num + 1):
-        filename = f"{books_folder}//id{num}.txt"
-        url = f"http://tululu.org/txt.php?id={num}"
-        response = requests.get(url, verify=False)
-        response.raise_for_status()
-        try:
-            check_for_redirect(response)
-            print(response.url)
-        except requests.HTTPError:
-            continue
-        with open(filename, "w") as outfile:
-            outfile.write(response.text)
-
-
-def download_txt(url, filename, folder='books/'):
-    books_folder = pathlib.Path(folder)
-    book_name = f"{filename}.txt"
-    books_folder.mkdir(parents=True, exist_ok=True)
+def download_txt(url, filename, folder="books/"):
+    book_file = f"{filename}.txt"
     response = requests.get(url)
     response.raise_for_status()
-    filepath = os.path.join(books_folder, sanitize_filename(book_name))
+    check_for_redirect(response)
+    filepath = os.path.join(folder, sanitize_filename(book_file))
     with open(filepath, "w") as outfile:
         outfile.write(response.text)
-    return filepath
 
-
+    
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    # try:
-    #     download_books(10)
-    # except requests.exceptions.HTTPError as err:
-    #     print("General Error, incorrect link\n", str(err))
-    # except requests.ConnectionError as err:
-    #     print("Connection Error. Check Internet connection.\n", str(err))
-    url = 'http://tululu.org/txt.php?id=1'
-
-    filepath = download_txt(url, 'Алиби')
-    print(filepath)  # Выведется books/Алиби.txt
-    
-    filepath = download_txt(url, 'Али/би', folder='books/')
-    print(filepath)  # Выведется books/Алиби.txt
-    
-    filepath = download_txt(url, 'Али\\би', folder='txt/')
-    print(filepath)  # Выведется txt/Алиби.txt
+    books_folder = pathlib.Path("books/")
+    books_folder.mkdir(parents=True, exist_ok=True)
+    try:
+        for num in range(1, 11):
+            book_url = f"http://tululu.org/txt.php?id={num}"
+            book_page = f"https://tululu.org/b{num}"
+            book_title = f"{num}. {get_book_title(book_page)}"
+            try:
+                download_txt(book_url, book_title, books_folder)
+            except requests.HTTPError:
+                continue
+    except requests.exceptions.HTTPError as err:
+        print("General Error, incorrect link\n", str(err))
+    except requests.ConnectionError as err:
+        print("Connection Error. Check Internet connection.\n", str(err))
 
 
 if __name__=="__main__":
